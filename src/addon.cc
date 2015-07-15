2aec06e0c59791d8ead28a80a2c99c16c90fa07c
bradleymeck <bradley.meck@gmail.com>
2015-07-15 10:50:44 -0500

    now taking in the str from the main thread

--- src/addon.cc
+++ src/addon.cc
@@ -1,70 +1,80 @@
 // addon.cc
+#include <stdio.h>
 #include <node.h>
 #include <v8.h>
 #include <threx.h>
-#include <stdio.h>
 #include <pthread.h>
 
 using namespace v8;
 using threx::thread_resource_t;
 using threx::export_work;
 
+// structure used for performing work on the thread
+// needs a callback to let us get results from the thread
 struct EvalWork {
   Persistent<Function> callback;
   int length;
   char* src;
 };
 
-void parseCb(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = args.GetIsolate();
-  HandleScope scope(isolate);
-  uint64_t tid;
-  pthread_threadid_np(NULL, &tid);
-  printf("parseCb %d\n", tid);
-  args.GetReturnValue().Set(args[0]);
-}
-
+// called on the main thread to queue work safely on the loop
 static void parse(thread_resource_t* tr, void* data, size_t size) {
   Isolate* isolate = Isolate::GetCurrent();
-  EvalWork* work = (EvalWork*)data;
+
   uint64_t tid;
   pthread_threadid_np(NULL, &tid);
   printf("parse %d\n", tid);
+
+  EvalWork* work = (EvalWork*)data;
   Local<Function> fn = *reinterpret_cast<v8::Local<Function>*>(
           const_cast<v8::Persistent<Function>*>(&(work->callback)));
+
+  // cleanup
   work->callback.Reset();
+  free(work->src);
+  free(work);
+
+  // call out cb
   int argc = 1;
   Handle<String> str = String::NewFromUtf8(isolate, work->src, String::NewStringType::kNormalString, work->length);
   Local<Value> argv[] = {str};
   node::MakeCallback(isolate, v8::Object::New(isolate), fn, argc, argv);
 }
 
+// used on the thread to perform some work
+// currently: printf the str passed in, send it back to main
+//   via parse
 static void test_cb(thread_resource_t* tr, void* data, size_t size) {
   EvalWork* work = (EvalWork*)data;
   uint64_t tid;
   pthread_threadid_np(NULL, &tid);
-  printf("text_cb %d\n", tid);
   // send same ptr back to main thread
+  printf("work.src %s\n", work->src);
   threx::enqueue_cb(tr, parse, work); 
 }
 
+// JS binding to create the work plan
+// DOES NOT EXECUTE WORK
 void RunMe(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
-  Handle<Function> fn = Handle<Function>::Cast(args[1]);
+
   EvalWork* work = new EvalWork();
+  Handle<Function> fn = Handle<Function>::Cast(args[0]);
   work->callback.Reset(isolate, fn);
-  work->src = "123";
-  work->length = 3;
-  uint64_t tid;
-  pthread_threadid_np(NULL, &tid);
-  printf("RunMe %d\n", tid);
-  args.GetReturnValue().Set(export_work(isolate, test_cb));
+  String::Utf8Value utf(args[1]->ToString());
+  work->length = utf.length();
+  work->src = (char*)malloc(work->length);
+  memcpy(work->src, *utf, work->length);
+
+  // return our work and the external to pass it
+  // external includes the cb for the main thread
+  Handle<Array> arr = Array::New(isolate, 2);
+  arr->Set(0, export_work(isolate, test_cb));
+  arr->Set(1, External::New(isolate, reinterpret_cast<void*>(work)));
+  args.GetReturnValue().Set(arr);
 }
 
 void InitAll(Handle<Object> exports) {
-  uint64_t tid;
-  pthread_threadid_np(NULL, &tid);
-  printf("InitAll %d\n", tid);
   NODE_SET_METHOD(exports, "eval", RunMe);
 }
 
--- test.js
+++ test.js
@@ -1,8 +1,17 @@
 var Threx = require('threx');
-var work = require('./build/Release/worky.node').eval(function () {console.log(arguments);},'test')
+function log() {
+  console.log('log:', arguments);
+}
+var worky = require('./build/Release/worky.node');
+
+// eval(cb, string)
+var work_parts = worky.eval(log, 'tess')
+
 var thread = new Threx();
 thread.spawn();
 console.log('queueing');
-thread.enqueue(work);
+thread.enqueue(work_parts[0], work_parts[1]);
+// should printf here for parse?
 thread.join();
 console.log('joined');
+// should log: here
